018195a3700a180ad7b3b3af7486df73debd3ac3
josevictorsss <josevictortf@gmail.com>
2022-12-04 23:34:03 -0300

    feat: modal implemented to check orders

--- app/src/components/OrdersBoard/index.tsx
+++ app/src/components/OrdersBoard/index.tsx
@@ -1,6 +1,8 @@
 import { Board, OrdersContainer } from "./styles";
 
 import { Order } from "../../types/Order";
+import { useState } from "react";
+import { OrderModal } from "../OrderModal";
 
 interface OrdersBoardProps {
   icon: string;
@@ -9,23 +11,44 @@ interface OrdersBoardProps {
 }
 
 export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<
+    null | undefined | Order
+  >();
+
+  const handleOpenModal = (order: Order) => {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
+  };
+
   return (
     <Board>
+      <OrderModal visible={isModalVisible} order={selectedOrder} onClose={handleCloseModal} />
+
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
-        <span>(1)</span>
+        <span>({orders.length})</span>
       </header>
-      <OrdersContainer>
-        <button type="button">
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-        <button type="button">
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-      </OrdersContainer>
+      {orders.length > 0 && (
+        <OrdersContainer>
+          {orders.map((order) => (
+            <button
+              type="button"
+              key={order._id}
+              onClick={() => handleOpenModal(order)}
+            >
+              <strong>{order.table}</strong>
+              <span>{order.products.length}</span>
+            </button>
+          ))}
+        </OrdersContainer>
+      )}
     </Board>
   );
 }
